94cdf8584331cb037c8bff1453e0da598f7a4a63
Gauravshbrt <gauravsharma1991.gs@gmail.com>
2023-01-14 18:16:35 +0530

    Jan 23

---
+++ python prog/first prg.py
@@ -0,0 +1,15 @@
+print("my bane is vicky")
+print("tets one") 
+print("2*1=2")
+print("2*2=4")
+print("2*3=6")
+print("2*4=8")
+print("2*5=10")
+print("2*6=12")
+print("2*7=14")
+print("2*8=16")
+print("2*9=18")
+print("2*10=20")
+num= 12
+for i in range(1, 11):
+ print(num, 'x', i, '=', num*i)
---
+++ sam score card.jan.py
@@ -0,0 +1,7 @@
+print("vicky")
+print("test")
+
+
+
+
+
