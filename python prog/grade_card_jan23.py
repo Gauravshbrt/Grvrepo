315a8385759e2d14ea11ba8f38633175932e2336
Gauravshbrt <gauravsharma1991.gs@gmail.com>
2023-01-15 15:40:55 +0530

    15jan grade card

---
+++ python prog/grade_card_jan23.py
@@ -0,0 +1,36 @@
+int1 = int(input("Please enter 1st Interger: "))
+#int2 = int(input("Please enter 2nd integer: "))
+#multply = int1*int2
+#sum = int1+int2
+if int1 <90:
+    print(int1,"Rating--> (1) -->Need Improvement")
+   
+   #2nd condtn
+elif int1 >=90 and int1 <=100:
+	print(int1,"Rating ->>(2),,Meet Expedctation")
+
+#3rd condt
+elif int1 >100 and int1 <=105:
+	print(int1,"Rating ->>(3),,Exceed Expedctation")
+
+#4th condtn
+elif int1 > 105:
+   print(int1,"Rating-->(4) Outstanding")
+
+elif int1 == null: 
+    print("nul not allowed")
+
+
+#else:
+ #  print("Please enter valid Int")
+
+#	>90 & =<100 
+
+
+#	>100 & =<105 Rating--> 3 (Exceed Expectations)
+
+#	>105 Rating--> 4 (Outstanding)
+#x = "hello"
+
+#if not type(x) is int:
+ # raise TypeError("Only integers are allowed")
